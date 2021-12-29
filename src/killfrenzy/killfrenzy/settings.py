77dc540a30ea50a0164f8cf763e1292f13d9f88d
Christian Deacon <gamemann@gflclan.com>
2021-12-29 02:47:52 -0600

    Adjust settings.

--- src/killfrenzy/killfrenzy/settings.py
+++ src/killfrenzy/killfrenzy/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'connections.apps.ConnectionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'web_socket'
 ]
 
 MIDDLEWARE = [
