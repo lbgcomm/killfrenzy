dd16235cf08d37225871680627dddcb585ada715
Christian Deacon <gamemann@gflclan.com>
2022-03-15 00:02:13 +0000

    Do debug.

--- src/killfrenzy/connections/apps.py
+++ src/killfrenzy/connections/apps.py
@@ -3,14 +3,20 @@ import os
 
 import web_socket
 
+from django.conf import settings
+
 class ConnectionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'connections'
 
     def ready(self):
-        env = os.environ.get("WEBSERVER_SET")
+        if settings.DEBUG:
+            env = os.environ.get("WEBSERVER_SET")
 
-        if env is not None:
-            web_socket.socket_c.start()
+            if env is not None:
+                web_socket.socket_c.start()
+            else:
+                os.environ["WEBSERVER_SET"] = 'True'
         else:
             os.environ["WEBSERVER_SET"] = 'True'
+            web_socket.socket_c.start()
