4634399205ee91c44a7035fc9853b7cdfa98b70f
Christian Deacon <gamemann@gflclan.com>
2022-03-26 22:24:43 +0000

    Try default str.

--- src/killfrenzy/web_socket/web_socket.py
+++ src/killfrenzy/web_socket/web_socket.py
@@ -478,7 +478,7 @@ class Web_Socket(Thread):
             try:
                 async for data in client:
                     try:
-                        info = json.loads(data)
+                        info = json.loads(data, default=str)
                     except json.JSONDecodeError as e:
                         print("handler() :: Error handling JSON load.")
                         print("Error => " + e.msg)
