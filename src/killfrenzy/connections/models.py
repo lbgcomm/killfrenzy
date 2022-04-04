ab1a6c3ef147eb05674d1b38ee203c4935a4ad13
Christian Deacon <gamemann@gflclan.com>
2022-04-04 23:03:15 +0000

    Change name.

--- src/killfrenzy/connections/models.py
+++ src/killfrenzy/connections/models.py
@@ -314,7 +314,7 @@ class Port_Punch(models.Model):
     auto_added = models.BooleanField(verbose_name="Auto Added", help_text="Whether this was added by system", editable=False, default=False)
     ip = models.CharField(verbose_name="IP Address", help_text="IP address", max_length=32)
     port = models.IntegerField(verbose_name="Port", help_text="Port", default=0)
-    count = models.IntegerField(editable = False, default=0, null = True)
+    cnt = models.IntegerField(editable = False, default=0, null = True)
 
     service_ip = models.CharField(verbose_name="Service IP Address", help_text="Service IP address", max_length=32)
     service_port = models.IntegerField(verbose_name="Service Port", help_text="Service Port", default=0)
@@ -330,10 +330,10 @@ class Port_Punch(models.Model):
     def save(self, *args, **kwargs):
         new = False
 
-        if not self.id or (self.count is not None and self.count < 5):
+        if not self.id or (self.cnt is not None and self.cnt < 5):
             new = True
 
-        self.count = self.count + 1
+        self.cnt = self.count + 1
 
         super().save(*args, **kwargs)
 
